3be3188e07ebe16c7e8d08e04b3174b2344a0257
Rawad Al awar <ralawar54@gmail.com>
2016-09-12 08:15:57 +0000

    modify package.json

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,16 @@
-var hello="hello"
-console.log(hello)
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT
+
+app.get('/',function(req, res){
+    res.send("Aloha World")
+})
+
+app.get('/routing',function(req, res){
+    res.send("Aloha Routing")
+})
+
+app.listen(port, function(err){
+    console.log("The server is running on port: " + port);
+});
