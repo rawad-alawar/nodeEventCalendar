11f552497553ecc35d27e16ac062ff966e7a1002
Rawad Al awar <ralawar54@gmail.com>
2016-09-13 03:39:05 +0000

    set up ejs

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -4,11 +4,15 @@ var app = express();
 var port = process.env.PORT
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'))
 
+app.set('views','./src/views')
+app.set('view engine', 'ejs')
+
+
 app.get('/',function(req, res){
-    res.send("Aloha World")
+   // res.send("Aloha World")
+   res.render('index',{list: ['first val', 'second val', 'third val']})
 })
 
 app.get('/routing',function(req, res){
